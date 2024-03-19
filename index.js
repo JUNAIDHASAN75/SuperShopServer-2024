310f7d9e4f658e305a947cb0d80e54efc478ff83
Junaid Hasan <spiderss907@gmail.com>
2024-03-20 01:11:03 +0600

    create crud operation for carts,users

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-require('dotenv').config()
-const cors = require('cors');
+require("dotenv").config();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rm5ydmz.mongodb.net/?retryWrites=true&w=majority`;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rm5ydmz.mongodb.net/?retryWrites=true&w=majority`;
 // const uri = "mongodb+srv://<username>:<password>@cluster0.rm5ydmz.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -20,7 +18,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -28,13 +26,53 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const menuCollections = client.db("SuperShop2024").collection("menues");
-    app.get('/menues', async(req, res)=>{
-        const result = await menuCollections.find().toArray();
-        res.send(result);
-    })
+    const cartCollections = client.db("SuperShop2024").collection("carts");
+    const userCollections = client.db("SuperShop2024").collection("users");
+    app.get("/menues", async (req, res) => {
+      const result = await menuCollections.find().toArray();
+      res.send(result);
+    });
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await cartCollections.find(query).toArray();
+      res.send(result);
+    });
+    app.post("/carts", async (req, res) => {
+      const cartItem = req.body;
+      const result = await cartCollections.insertOne(cartItem);
+      res.send(result);
+    });
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = cartCollections.deleteOne(query);
+      res.send(result);
+    });
+
+    app.get("/users", async (req, res) => {
+      const result = await userCollections.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const query = req.body;
+      const result = await userCollections.insertOne(query);
+      res.send(result);
+    });
+
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollections.deleteOne(query);
+      res.send(result)
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -42,11 +80,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("super shop is running");
+});
 
-app.get('/', (req, res)=>{
-    res.send('super shop is running');
-})
-
-app.listen(port, ()=>{
-    console.log(`super shop server is running on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`super shop server is running on port ${port}`);
+});
