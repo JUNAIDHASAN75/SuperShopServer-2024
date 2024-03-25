3348b87b17cc2bc05a5a32fb54ebf3b9163b35a4
Junaid Hasan <spiderss907@gmail.com>
2024-03-25 06:18:50 +0600

    create vercel config file , and create user api

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ app.use(express.json());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rm5ydmz.mongodb.net/?retryWrites=true&w=majority`;
-// const uri = "mongodb+srv://<username>:<password>@cluster0.rm5ydmz.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -34,12 +33,40 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/menues/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await menuCollections.findOne(query);
+      res.send(result);
+    });
+
     app.post("/menues", async (req, res) => {
       const body = req.body;
       const result = await menuCollections.insertOne(body);
       res.send(result);
     });
 
+    app.put("/menues/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateItem = req.body;
+      const updateDoc = {
+        $set: {
+          name: updateItem.name,
+          price: updateItem.price,
+          image: updateItem.image,
+          category: updateItem.category,
+          details: updateItem.details,
+        },
+      };
+      const result = await menuCollections.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.send(result);
+    });
     app.delete("/menues/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
@@ -78,16 +105,36 @@ async function run() {
       const result = await userCollections.find().toArray();
       res.send(result);
     });
-    app.get("/users/:id", async(req,res)=>{
+    app.get("/users/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await userCollections.findOne(query);
       res.send(result);
-    })
+    });
+
+    app.get("/users/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await userCollections.findOne(query);
+      const result = { admin: user?.role === "admin" };
+      res.send(result);
+    });
 
     app.post("/users", async (req, res) => {
       const query = req.body;
       const result = await userCollections.insertOne(query);
+      res.send(result);
+    });
+
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await userCollections.updateOne(filter, updateDoc);
       res.send(result);
     });
 
